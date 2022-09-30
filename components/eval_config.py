e811ef5c00d75a88b83ba4e67a3ee5311644155a
GBhat2 <114494827+GBhat2@users.noreply.github.com>
2022-09-29 23:11:39 -0700

    Create eval_config.py

---
+++ components/eval_config.py
@@ -0,0 +1,48 @@
+import taxi_constants
+import sys
+if 'google.colab' in sys.modules:  # Testing to see if we're doing development
+  import importlib
+  importlib.reload(taxi_constants)
+
+eval_config = tfma.EvalConfig(
+    model_specs=[
+        # This assumes a serving model with signature 'serving_default'. If
+        # using estimator based EvalSavedModel, add signature_name: 'eval' and
+        # remove the label_key.
+        tfma.ModelSpec(
+            signature_name='serving_default',
+            label_key=taxi_constants.LABEL_KEY,
+            preprocessing_function_names=['transform_features'],
+            )
+        ],
+    metrics_specs=[
+        tfma.MetricsSpec(
+            # The metrics added here are in addition to those saved with the
+            # model (assuming either a keras model or EvalSavedModel is used).
+            # Any metrics added into the saved model (for example using
+            # model.compile(..., metrics=[...]), etc) will be computed
+            # automatically.
+            # To add validation thresholds for metrics saved with the model,
+            # add them keyed by metric name to the thresholds map.
+            metrics=[
+                tfma.MetricConfig(class_name='ExampleCount'),
+                tfma.MetricConfig(class_name='BinaryAccuracy',
+                  threshold=tfma.MetricThreshold(
+                      value_threshold=tfma.GenericValueThreshold(
+                          lower_bound={'value': 0.5}),
+                      # Change threshold will be ignored if there is no
+                      # baseline model resolved from MLMD (first run).
+                      change_threshold=tfma.GenericChangeThreshold(
+                          direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                          absolute={'value': -1e-10})))
+            ]
+        )
+    ],
+    slicing_specs=[
+        # An empty slice spec means the overall slice, i.e. the whole dataset.
+        tfma.SlicingSpec(),
+        # Data can be sliced along a feature column. In this case, data is
+        # sliced along feature column trip_start_hour.
+        tfma.SlicingSpec(
+            feature_keys=['trip_start_hour'])
+    ])
